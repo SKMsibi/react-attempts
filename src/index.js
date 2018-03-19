0f77f2b913ba4bd98f3cba92b8300b148030ff9a
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-03-19 14:06:36 +0200

    added a folder containing the mark down preview project

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from "react";
 import ReactDom from 'react-dom';
-import Archives from './pagesReactRouter/archives';
-import Featured from "./pagesReactRouter/featured";
-import Layout from './pagesReactRouter/initail'
-import Settings from './pagesReactRouter/settings'
-import { HashRouter, Route, Link } from 'react-router-dom';
-import { IndexRoute } from 'react-router'
+import MarkConvertor from './mark-down-project/mainPage';
 
 const app = document.getElementById("app")
-ReactDom.render(
-    <HashRouter>
-        <Route exact path="/" component={Layout} />
-        <Home>
-            <Route exact path="archives" component={Archives} />
-            <Route exact path="settings" component={Settings} />
-        </Home>
-    </HashRouter>,
-    app);
+ReactDom.render(<MarkConvertor />, app);
--- src/pagesReactRouter/initail.js
+++ src/pagesReactRouter/initail.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Settings from './settings'
 import { Link } from 'react-router-dom'
 export default class Layout extends React.Component {
     render() {
+        console.log(this.props)
         return (
             <div>
                 <h1>this is layout</h1>
-                {this.props.children}
                 <Link to="archives">archives</Link>
+                <Link to="settings">settings</Link>
+
             </div>
         )
     }
