63c45e168cc0a3382580e3ce184c9966dc081ea1
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-03-27 12:09:30 +0200

    added a function that shows and hides a list through a button

--- list/src/components/item.js
+++ list/src/components/item.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
 export default class Item extends React.Component {
-    
+    constructor() {
+        super();
+        this.state = { list: [] };
+        this.hideOrShow = "true";
+    }
+    handleButter() {
+        this.showAndHide();
+    }
+    showAndHide() {
+        console.log("data", this.hideOrShow)
+        if (this.hideOrShow === "true") {
+            this.setState({ list: this.props.product.map(element => { return <ul>{element}</ul> }) });
+            this.hideOrShow = "false";
+            console.log("data", this.results);
+        } else {
+            this.hideOrShow = "true";
+            this.setState({ list: <div></div> });
+        }
+    }
     render() {
-        console.log("data", this.props)
         return (
             <div>
-                <button>{this.props.item}</button>
-                {this.props.product.map(element => { return <ul>{element}</ul> })}
+                <button onClick={this.handleButter.bind(this)}>{this.props.item}</button>
+                {this.state.list}
             </div>
         )
     }
