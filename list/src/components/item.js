32afba2318dc140677282c37970627346efd3957
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-04-06 10:14:31 +0200

    changed the styling and how items are added to the list

--- list/src/components/item.js
+++ list/src/components/item.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../index.css';
 
 export default class Item extends React.Component {
     constructor(props) {
         super();
-        this.state = { list: [], del: [] };
+        this.state = { list: [], del: [], edit: [] };
         this.hideOrShow = "true";
     }
     handleButter() {
@@ -11,24 +12,43 @@ export default class Item extends React.Component {
     }
     showAndHide() {
         if (this.hideOrShow === "true") {
-            this.setState({ list: this.props.product.map(element => { return <ul>{element}</ul> }) });
-            this.setState({ del: <button onClick={() => this.deleteItem()}>Delete {this.props.item}</button> });
-            this.hideOrShow = "false";
+            try {
+                this.setState({ list: this.props.product.map(element => { return <li>{element}</li> }) });
+                this.setState({ del: <button id="deleteButton" onClick={() => this.deleteItem()}>Delete {this.props.item}</button> });
+                this.setState({ edit: <button id="editButton" onClick={() => this.editItem()}>Edit Items</button> })
+
+                this.hideOrShow = "false";
+            } catch (error) {
+                this.setState({ list: "this list is empty" });
+                this.setState({ del: <button id="deleteButton" onClick={() => this.deleteItem()}>Delete</button> });
+                this.setState({ edit: <button id="editButton" onClick={() => this.editItem()}>Edit Items</button> })
+
+            }
         } else {
             this.hideOrShow = "true";
             this.setState({ list: <div></div> });
             this.setState({ del: <div></div> })
+            this.setState({ edit: <div></div> })
         }
     }
     deleteItem() {
         this.props.deleteButton(this.props.item)
     }
+    editItem() {
+        this.props.editButton(this.props.product, this.props.item);
+
+    }
     render() {
         return (
             <div>
-                <button onClick={this.handleButter.bind(this)}>{this.props.item}</button>
-                {this.state.list}
+                <button class="btn btn-primary" onClick={this.handleButter.bind(this)} id="ItemButton">{this.props.item}</button>
+                <div className="container">
+                    <ul>
+                        {this.state.list}
+                    </ul>
+                </div>
                 {this.state.del}
+                {this.state.edit}
             </div>
         )
     }
--- list/src/components/mainPage.js
+++ list/src/components/mainPage.js
@@ -23,16 +23,15 @@ export default class Layout extends React.Component {
             this.state.allItems.push(this.singleItem);
             this.setState({ allItems: this.state.allItems })
             this.singleItem = {};
-            localStorage.setItem("data", JSON.stringify(this.state.allItems));
+            localStorage.setItem("valuedRecipes", JSON.stringify(this.state.allItems));
         }
     }
     componentDidMount() {
-        var item = localStorage.getItem("data");
+        var item = localStorage.getItem("valuedRecipes");
         var hint = JSON.parse(item);
         var list = [];
         try {
             hint.forEach(element => {
-                this.setState()
                 list.push(element)
                 this.setState({ allItems: list })
             });
@@ -44,23 +43,41 @@ export default class Layout extends React.Component {
             if (element.item === itemName) {
                 this.state.allItems.splice(this.state.allItems.indexOf(element), this.state.allItems.indexOf(element) + 1)
                 this.setState({ allItems: this.state.allItems })
-                localStorage.setItem("data", JSON.stringify(this.state.allItems));
+                localStorage.setItem("valuedRecipes", JSON.stringify(this.state.allItems));
+            }
+        })
+    }
+    editList(itemList, item) {
+        var newItems = prompt("You can change you Item", itemList);
+        this.state.allItems.forEach(element => {
+            if (element.item === item) {
+                try {
+                    var index = this.state.allItems.indexOf(element);
+                    this.state.allItems.splice(index, index + 1);
+                    this.singleItem = this.state.allItems.splice(index, index, { "item": item, product: newItems.split(",") });
+                    localStorage.setItem("valuedRecipes", JSON.stringify(this.state.allItems));
+                    window.location.reload(true)
+                } catch (error) {
+
+                }
             }
         })
     }
     render() {
         return (
             <div>
-                <h1>This is the main page</h1>
+                <h1>Recipe Box</h1>
                 <div>
-                    <input type="text" placeholder="final Product e.g Cake" onChange={this.eventHandler.bind(this)} /> |||
-                    <input type="text" placeholder="Ingredients e.g milk,eggs" onChange={this.eventHandlerT.bind(this)} /><br />
-                    <button onClick={this.handleButton.bind(this)}>Add</button><br />
+                    <h4>Recipe name</h4>
+                    <input type="text" id="inputText" placeholder="e.g chocolate Cake" onChange={this.eventHandler.bind(this)} /> <br /><br />
+                    <h4>Recipe items</h4>
+                    <textarea placeholder="e.g baking Powder,chocolate flavouring,milk" onChange={this.eventHandlerT.bind(this)} ></textarea><br />
+                    <button class="btn btn-primary" onClick={this.handleButton.bind(this)}>Add</button><br />
                 </div>
                 <div>
                     {this.state.allItems.map(element => {
-                        return <div class="container">
-                            <Item item={element.item} product={element.product} status={element.status} deleteButton={this.deleteItem.bind(this)} />
+                        return <div className="container-fluid">
+                            <Item key={this.state.allItems.indexOf(element)} item={element.item} test="this is a test" product={element.product} status={element.status} deleteButton={this.deleteItem.bind(this)} editButton={this.editList.bind(this)} />
                         </div>
                     })}
                 </div>
