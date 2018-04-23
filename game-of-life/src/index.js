f4fc41c29d16a567a1a9e4e2ef71d6a64d90ff02
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-04-23 19:00:29 +0200

    added generation number and game status displayers and determiners

--- game-of-life/src/index.js
+++ game-of-life/src/index.js
@@ -10,7 +10,7 @@ class Cell extends React.Component {
         this.recursiveGenerationGenerator = this.recursiveGenerationGenerator.bind(this);
     }
     componentDidMount() {
-        this.setState({ grid: generateNextGeneration() })
+        this.setState({ grid: generateNextGeneration(), generationNumber: 1, gameStatus: "On" })
     }
     bringToLifeOrTakeAwayLIfe(cell) {
         var positionOfCell = this.state.grid.indexOf(cell);
@@ -20,25 +20,32 @@ class Cell extends React.Component {
             this.state.grid[positionOfCell].status = true;
         }
         this.setState({ grid: this.state.grid })
-        console.log("positionOfCell", positionOfCell, cell);
     }
 
     recursiveGenerationGenerator() {
         var generationNumber = 0;
         var workedOnGeneration = [];
         var generatorLoop = setInterval(() => {
-            workedOnGeneration = generateNextGeneration(this.state.grid);
-            this.setState({ grid: workedOnGeneration })
             generationNumber++;
-        }, 3000);
-        if (generationNumber === 5) {
-            clearTimeout(generatorLoop);
-        }
+            workedOnGeneration = generateNextGeneration(this.state.grid);
+            this.setState({ grid: workedOnGeneration, generationNumber: generationNumber })
+            var existenceOfAliveCell = this.state.grid.find(element => { return element.status === true });
+
+            if (generationNumber === 5) {
+                clearInterval(generatorLoop);
+                this.setState({ gameStatus: "Over" });
+            } else if (existenceOfAliveCell === undefined) {
+                clearInterval(generatorLoop);
+                this.setState({ gameStatus: "Over" });
+            }
+        }, 2000);
     }
     render() {
         return (
             <div>
                 <button className='btn btn-primary' onClick={() => this.recursiveGenerationGenerator()}>Start</button>
+                <span>{this.state.generationNumber}</span>
+                <span>Game: {this.state.gameStatus}</span>
                 <div id="grid">
                     {
                         this.state.grid.map(gridCell => {
@@ -50,6 +57,5 @@ class Cell extends React.Component {
         )
     }
 }
-
 const app = document.getElementById("app")
 ReactDom.render(<Cell />, app);
