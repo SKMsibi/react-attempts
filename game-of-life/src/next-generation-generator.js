d4d299fa0b2e5c53b29024fdc1f3ffd487855348
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-04-23 09:35:57 +0200

    expanded the grid size, change the results in the tests to accomodate the grid increase,added styling for alive cells in the css file and added bootstrap on the html page

--- game-of-life/src/next-generation-generator.js
+++ game-of-life/src/next-generation-generator.js
@@ -1,59 +1,119 @@
+
 function generateNextGeneration(currentGeneration) {
     var newGeneration = [];
-    var WorkedOnCurrentGeneration = currentGeneration;
-    var bored = [];
-    if (WorkedOnCurrentGeneration === undefined) {
+    if (currentGeneration === undefined) {
         bored = [{ xAxes: 0, yAxes: 0, status: false },
         { xAxes: 0, yAxes: 1, status: false },
         { xAxes: 0, yAxes: 2, status: false },
         { xAxes: 0, yAxes: 3, status: false },
         { xAxes: 0, yAxes: 4, status: false },
         { xAxes: 0, yAxes: 5, status: false },
+        { xAxes: 0, yAxes: 6, status: false },
+        { xAxes: 0, yAxes: 7, status: false },
+        { xAxes: 0, yAxes: 8, status: false },
+        { xAxes: 0, yAxes: 9, status: false },
         { xAxes: 1, yAxes: 0, status: false },
-        { xAxes: 1, yAxes: 1, status: true },
-        { xAxes: 1, yAxes: 2, status: true },
-        { xAxes: 1, yAxes: 3, status: true },
+        { xAxes: 1, yAxes: 1, status: false },
+        { xAxes: 1, yAxes: 2, status: false },
+        { xAxes: 1, yAxes: 3, status: false },
         { xAxes: 1, yAxes: 4, status: false },
         { xAxes: 1, yAxes: 5, status: false },
+        { xAxes: 1, yAxes: 6, status: false },
+        { xAxes: 1, yAxes: 7, status: false },
+        { xAxes: 1, yAxes: 8, status: false },
+        { xAxes: 1, yAxes: 9, status: false },
         { xAxes: 2, yAxes: 0, status: false },
         { xAxes: 2, yAxes: 1, status: false },
         { xAxes: 2, yAxes: 2, status: false },
         { xAxes: 2, yAxes: 3, status: false },
         { xAxes: 2, yAxes: 4, status: false },
         { xAxes: 2, yAxes: 5, status: false },
+        { xAxes: 2, yAxes: 6, status: false },
+        { xAxes: 2, yAxes: 7, status: false },
+        { xAxes: 2, yAxes: 8, status: false },
+        { xAxes: 2, yAxes: 9, status: false },
         { xAxes: 3, yAxes: 0, status: false },
         { xAxes: 3, yAxes: 1, status: false },
         { xAxes: 3, yAxes: 2, status: false },
         { xAxes: 3, yAxes: 3, status: false },
-        { xAxes: 3, yAxes: 4, status: false },
-        { xAxes: 3, yAxes: 5, status: false },
+        { xAxes: 3, yAxes: 4, status: true },
+        { xAxes: 3, yAxes: 5, status: true },
+        { xAxes: 3, yAxes: 6, status: true },
+        { xAxes: 3, yAxes: 7, status: false },
+        { xAxes: 3, yAxes: 8, status: false },
+        { xAxes: 3, yAxes: 9, status: false },
         { xAxes: 4, yAxes: 0, status: false },
         { xAxes: 4, yAxes: 1, status: false },
-        { xAxes: 4, yAxes: 2, status: true },
+        { xAxes: 4, yAxes: 2, status: false },
         { xAxes: 4, yAxes: 3, status: false },
         { xAxes: 4, yAxes: 4, status: false },
         { xAxes: 4, yAxes: 5, status: false },
+        { xAxes: 4, yAxes: 6, status: false },
+        { xAxes: 4, yAxes: 7, status: false },
+        { xAxes: 4, yAxes: 8, status: false },
+        { xAxes: 4, yAxes: 9, status: false },
         { xAxes: 5, yAxes: 0, status: false },
         { xAxes: 5, yAxes: 1, status: false },
-        { xAxes: 5, yAxes: 2, status: true },
-        { xAxes: 5, yAxes: 3, status: true },
+        { xAxes: 5, yAxes: 2, status: false },
+        { xAxes: 5, yAxes: 3, status: false },
         { xAxes: 5, yAxes: 4, status: false },
-        { xAxes: 5, yAxes: 5, status: false }];
+        { xAxes: 5, yAxes: 5, status: false },
+        { xAxes: 5, yAxes: 6, status: false },
+        { xAxes: 5, yAxes: 7, status: false },
+        { xAxes: 5, yAxes: 8, status: false },
+        { xAxes: 5, yAxes: 9, status: false },
+        { xAxes: 6, yAxes: 0, status: false },
+        { xAxes: 6, yAxes: 1, status: false },
+        { xAxes: 6, yAxes: 2, status: false },
+        { xAxes: 6, yAxes: 3, status: false },
+        { xAxes: 6, yAxes: 4, status: false },
+        { xAxes: 6, yAxes: 5, status: false },
+        { xAxes: 6, yAxes: 6, status: false },
+        { xAxes: 6, yAxes: 7, status: false },
+        { xAxes: 6, yAxes: 8, status: false },
+        { xAxes: 6, yAxes: 9, status: false },
+        { xAxes: 7, yAxes: 0, status: false },
+        { xAxes: 7, yAxes: 1, status: false },
+        { xAxes: 7, yAxes: 2, status: false },
+        { xAxes: 7, yAxes: 3, status: false },
+        { xAxes: 7, yAxes: 4, status: false },
+        { xAxes: 7, yAxes: 5, status: false },
+        { xAxes: 7, yAxes: 6, status: false },
+        { xAxes: 7, yAxes: 7, status: false },
+        { xAxes: 7, yAxes: 8, status: false },
+        { xAxes: 7, yAxes: 9, status: false },
+        { xAxes: 8, yAxes: 0, status: false },
+        { xAxes: 8, yAxes: 1, status: false },
+        { xAxes: 8, yAxes: 2, status: false },
+        { xAxes: 8, yAxes: 3, status: false },
+        { xAxes: 8, yAxes: 4, status: true },
+        { xAxes: 8, yAxes: 5, status: true },
+        { xAxes: 8, yAxes: 6, status: false },
+        { xAxes: 8, yAxes: 7, status: false },
+        { xAxes: 8, yAxes: 8, status: false },
+        { xAxes: 8, yAxes: 9, status: false },
+        { xAxes: 9, yAxes: 0, status: false },
+        { xAxes: 9, yAxes: 1, status: false },
+        { xAxes: 9, yAxes: 2, status: false },
+        { xAxes: 9, yAxes: 3, status: false },
+        { xAxes: 9, yAxes: 4, status: true },
+        { xAxes: 9, yAxes: 5, status: true },
+        { xAxes: 9, yAxes: 6, status: false },
+        { xAxes: 9, yAxes: 7, status: false },
+        { xAxes: 9, yAxes: 8, status: false },
+        { xAxes: 9, yAxes: 9, status: false }];
     } else {
         bored = currentGeneration;
     }
     bored.forEach(element => {
         var changedElement = { xAxes: element.xAxes, yAxes: element.yAxes, status: element.status }
         var aliveNeighbors = [];
-        var deadNeighbors = [];
         var elementNeighbors = getAllNeighbors(element);
         elementNeighbors.forEach(eachItem => {
             for (var i = 0; i < bored.length; i++) {
                 if (bored[i].xAxes === eachItem[0] && bored[i].yAxes === eachItem[1]) {
                     if (bored[i].status) {
                         aliveNeighbors.push(bored[i])
-                    } else {
-                        deadNeighbors.push(bored[i])
                     }
                 }
             }
