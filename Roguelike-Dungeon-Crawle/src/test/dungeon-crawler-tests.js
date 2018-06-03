690fe54827a3d5d91aede4e62b74e234b1db9d2b
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-06-03 22:31:10 +0200

    added the grid creation tests

--- Roguelike-Dungeon-Crawle/src/test/dungeon-crawler-tests.js
+++ Roguelike-Dungeon-Crawle/src/test/dungeon-crawler-tests.js
@@ -1,7 +1,8 @@
 var assert = require('assert');
 var expect = require("chai").expect;
 var nextStageGenerator = require("../components/game-layout");
-var pathWays = require("../components/game-stage")
+var pathWays = require("../components/game-stage");
+var weapons = [{ title: "Dagger", impact: 15, emoji: "em em-dagger_knife" }, { title: "Samurai Sword", impact: 25, emoji: "em em-crossed_swords" }, { title: "Gun", impact: 40, emoji: "em em-gun" }, { title: "Bomb", impact: 65, emoji: "em em-bomb" }];
 describe('The dungeon crawler game tests', function () {
     describe("Testing for the creation of the boss", function () {
         it("should return the type of object", function () {
@@ -18,5 +19,13 @@ describe('The dungeon crawler game tests', function () {
         it("should return the requested stage", function () {
             expect(pathWays.stageOne).to.be.eql(pathWays.stageOne)
         });
+    })
+    describe("Entire grid creation tests", function () {
+        it("should return the type of array", function () {
+            expect(typeof nextStageGenerator.generateGameLayout(pathWays.stageOne, [{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }, { xAxis: 2, yAxis: 7 }], [{ xAxis: 4, yAxis: 5 }], [{ xAxis: 5, yAxis: 3 }, { xAxis: 7, yAxis: 4 }, { xAxis: 2, yAxis: 5 }], { xAxis: 5, yAxis: 7 }, boss = { xAxis: 3, yAxis: 3 }, 1)).to.be.eql("object");
+        })
+        it("should generate an array with length of 100", function () {
+            expect(nextStageGenerator.generateGameLayout(pathWays.stageOne, [{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }, { xAxis: 2, yAxis: 7 }], [{ xAxis: 4, yAxis: 5 }], [{ xAxis: 5, yAxis: 3 }, { xAxis: 7, yAxis: 4 }, { xAxis: 2, yAxis: 5 }], { xAxis: 5, yAxis: 7 }, boss = { xAxis: 3, yAxis: 3 }, 1).length).to.be.eql(100);
+        })
     })
 })
