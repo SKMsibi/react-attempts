d8bfd395ca7867665181ed002bf0eef4ffccd649
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-06-03 21:13:33 +0200

    added a test folder and a test for creating a boss for the game

---
+++ Roguelike-Dungeon-Crawle/src/test/dungeon-crawler-tests.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var expect = require("chai").expect;
+var nextStageGenerator = require("../components/game-layout");
+
+describe('The dungeon crawler game tests', function () {
+    it("should return the type of object", function () {
+        expect(typeof nextStageGenerator.createTheBoss([{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }, { xAxis: 2, yAxis: 7 }])).to.be.eql("object")
+    })
+})
