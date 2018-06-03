ea798aebde9b3dc35ea5de28471a03783d07a4d3
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-06-03 21:20:53 +0200

    added a test for stages

--- Roguelike-Dungeon-Crawle/src/test/dungeon-crawler-tests.js
+++ Roguelike-Dungeon-Crawle/src/test/dungeon-crawler-tests.js
@@ -1,13 +1,15 @@
 var assert = require('assert');
 var expect = require("chai").expect;
 var nextStageGenerator = require("../components/game-layout");
-
+var pathWays = require("../components/game-stage")
 describe('The dungeon crawler game tests', function () {
     it("should return the type of object", function () {
         expect(typeof nextStageGenerator.createTheBoss([{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }, { xAxis: 2, yAxis: 7 }])).to.be.eql("object")
-    })
+    });
     it("return an object being the boss location in the last stage", function () {
         expect(nextStageGenerator.createTheBoss([{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }, { xAxis: 2, yAxis: 7 }])).to.be.eql({ boss: { xAxis: 2, yAxis: 7 }, newEnemies: [{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }] })
-    })
-
+    });
+    it("should return the requested stage", function () {
+        expect(pathWays.stageOne).to.be.eql(pathWays.stageOne)
+    });
 })
