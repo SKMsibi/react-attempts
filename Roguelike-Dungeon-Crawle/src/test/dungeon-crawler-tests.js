f554bfb3c5f00bea4e6c35c29bbdf684f6627b27
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-06-03 21:17:06 +0200

    added another test case that returns a boss and enemies =

--- Roguelike-Dungeon-Crawle/src/test/dungeon-crawler-tests.js
+++ Roguelike-Dungeon-Crawle/src/test/dungeon-crawler-tests.js
@@ -6,4 +6,8 @@ describe('The dungeon crawler game tests', function () {
     it("should return the type of object", function () {
         expect(typeof nextStageGenerator.createTheBoss([{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }, { xAxis: 2, yAxis: 7 }])).to.be.eql("object")
     })
+    it("return an object being the boss location in the last stage", function () {
+        expect(nextStageGenerator.createTheBoss([{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }, { xAxis: 2, yAxis: 7 }])).to.be.eql({ boss: { xAxis: 2, yAxis: 7 }, newEnemies: [{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }] })
+    })
+
 })
