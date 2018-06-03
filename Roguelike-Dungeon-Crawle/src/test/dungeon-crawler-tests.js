83798dceb62832741f0672521f371d697339bcce
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-06-03 21:42:59 +0200

    separated tests into sections under the same file

--- Roguelike-Dungeon-Crawle/src/test/dungeon-crawler-tests.js
+++ Roguelike-Dungeon-Crawle/src/test/dungeon-crawler-tests.js
@@ -3,13 +3,20 @@ var expect = require("chai").expect;
 var nextStageGenerator = require("../components/game-layout");
 var pathWays = require("../components/game-stage")
 describe('The dungeon crawler game tests', function () {
-    it("should return the type of object", function () {
-        expect(typeof nextStageGenerator.createTheBoss([{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }, { xAxis: 2, yAxis: 7 }])).to.be.eql("object")
-    });
-    it("return an object being the boss location in the last stage", function () {
-        expect(nextStageGenerator.createTheBoss([{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }, { xAxis: 2, yAxis: 7 }])).to.be.eql({ boss: { xAxis: 2, yAxis: 7 }, newEnemies: [{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }] })
-    });
-    it("should return the requested stage", function () {
-        expect(pathWays.stageOne).to.be.eql(pathWays.stageOne)
-    });
+    describe("Testing for the creation of the boss", function () {
+        it("should return the type of object", function () {
+            expect(typeof nextStageGenerator.createTheBoss([{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }, { xAxis: 2, yAxis: 7 }])).to.be.eql("object")
+        });
+        it("should return an object being the boss location in the last stage", function () {
+            expect(nextStageGenerator.createTheBoss([{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }, { xAxis: 2, yAxis: 7 }])).to.be.eql({ boss: { xAxis: 2, yAxis: 7 }, newEnemies: [{ xAxis: 3, yAxis: 3 }, { xAxis: 5, yAxis: 4 }] })
+        });
+    })
+    describe("Pathways testing", function () {
+        it("should return the type of object", function () {
+            expect(typeof pathWays.stageOne).to.be.eql("object")
+        })
+        it("should return the requested stage", function () {
+            expect(pathWays.stageOne).to.be.eql(pathWays.stageOne)
+        });
+    })
 })
