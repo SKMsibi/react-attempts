3a6da2264e26e85392074b87abdfbbfd3305b365
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-06-25 12:05:45 +0200

    added the emojies for the snake head, snake body and the point being the rat. I also create the build folder

--- snake-game/src/components/display-grid.js
+++ snake-game/src/components/display-grid.js
@@ -4,7 +4,17 @@ export default class DisplayGrid extends Component {
         return (
             <div id="grid">
                 {this.props.grid.map(element => {
-                    return <span key={this.props.grid.indexOf(element)} id={element.occupied}>{element.occupied}</span>
+                    if (element.occupied === "snakeHead") {
+                        element.display = <i class="em em-dragon_face"></i>;
+                    }
+                    else if (element.occupied === "point") {
+                        element.display = <i class="em em-rat"></i>;
+                    } else if (element.occupied === "snakeBody") {
+                        element.display = <i class="em em-snake"></i>;
+                    } else {
+                        element.display = element.occupied
+                    }
+                    return <span key={this.props.grid.indexOf(element)} id={element.occupied}>{element.display}</span>
                 })}
             </div>
         )
