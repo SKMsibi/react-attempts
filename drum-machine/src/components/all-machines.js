34efff91ef0415e837fd2ecff31a114c6f8acdb8
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-08-16 23:27:04 +0200

    created a component that displayes all available machines.

---
+++ drum-machine/src/components/all-machines.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as action from '../actions/allActions';
+export class AllMachines extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentMachine: props.name,
+            machineSounds: props.sounds
+        }
+        this.changeCurrentMachine = this.changeCurrentMachine.bind(this)
+    }
+    changeCurrentMachine(chosen) {
+        if (this.props.status) {
+            this.setState({ currentMachine: chosen })
+            this.props.changeMachine(chosen);
+            this.props.changeSounds(chosen);
+        }
+    }
+    componentWillReceiveProps() {
+        this.setState({ currentMachine: this.props.name, machineSounds: this.props.sounds })
+    }
+    render() {
+        return (
+            <div className="container" id="machine-container">
+                <button className={this.props.name === "Guitar" ? "btn btn-warning" : "btn btn-default"} onClick={() => this.changeCurrentMachine("Guitar")}>Guitar</button>
+                <button className={this.props.name === "Drum" ? "btn btn-warning" : "btn btn-default"} onClick={() => this.changeCurrentMachine("Drum")}>Drum</button>
+                <button className={this.props.name === "Piano" ? "btn btn-warning" : "btn btn-default"} onClick={() => this.changeCurrentMachine("Piano")}>Piano</button>
+            </div>
+        )
+    }
+}
+function mapStateToProps(state) {
+    return { status: state.machineReducer.AppStatus, name: state.machineReducer.currentMachineName, sounds: state.machineReducer.currentMachineSounds };
+}
+const mapDispatchToProps = dispatch => ({
+    changeMachine: (name) => dispatch(action.changeCurrentMachine(name)),
+    changeSounds: (machine) => dispatch(action.changeCurrentMachineSounds(machine))
+})
+export default connect(mapStateToProps, mapDispatchToProps)(AllMachines);
