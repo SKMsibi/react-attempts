e282c0a67391e9b1773140e1d1c0868f40d3471a
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-08-22 22:49:18 +0200

    imported sound files into one file.

---
+++ drum-machine/src/sounds/all-sounds.js
@@ -0,0 +1,46 @@
+var marimba = require("./drum/marimba.wav");
+var tabla = require("./drum/tabla.mp3");
+var cajonBass = require("./drum/cajon-bass.mp3");
+var hietom = require("./drum/hietom.mp3");
+var hitom = require("./drum/hitom.mp3");
+var marioCoin = require("./drum/mario-coin.mp3");
+var marioJump = require("./drum/mario-jump.wav");
+var steel = require("./drum/steel.mp3")
+
+var base = require("./guitar/base.mp3");
+var countryChord = require("./guitar/country-chord.mp3");
+var fuzzyNote = require("./guitar/fuzzy-note.mp3");
+var guitarPluck = require("./guitar/guitar-pluck.mp3");
+var shortWarm = require("./guitar/short-warm.mp3");
+
+var bluesPiano = require("./piano/blues-piano.mp3");
+var darkHouse = require("./piano/dark-house.mp3");
+var dirtyHigh = require("./piano/dirty-high.mp3");
+var fugged = require("./piano/fugged.mp3");
+var organLow = require("./piano/organ-low.mp3");
+var superCheez = require("./piano/super-cheez.mp3");
+var thickChord = require("./piano/thick-chord.mp3");
+var twoTone = require("./piano/two-tone.mp3");
+module.exports = {
+    marimba,
+    tabla,
+    cajonBass,
+    hietom,
+    hitom,
+    marioCoin,
+    marioJump,
+    steel,
+    base,
+    countryChord,
+    fuzzyNote,
+    guitarPluck,
+    shortWarm,
+    bluesPiano,
+    darkHouse,
+    dirtyHigh,
+    fugged,
+    organLow,
+    superCheez,
+    thickChord,
+    twoTone,
+}
