efead9e621294a1f5a2065f84823e3e5473c2db4
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-08-22 13:08:57 +0200

    removed last played from state. added code that reads last played from the redux store.

--- drum-machine/src/components/machine-keys.js
+++ drum-machine/src/components/machine-keys.js
@@ -1,38 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import "../App.css";
+import * as actions from '../actions/allActions';
 import LastPlayed from './last-played-sound';
 class MachineKeys extends Component {
     constructor(props) {
         super(props)
         this.state = {
             buttons: ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"],
-            lastPlayed: ""
         }
         this.playSound = this.playSound.bind(this);
     }
     playSound(soundName) {
-        this.setState({ lastPlayed: soundName })
+        this.props.changeLastPlayed(soundName);
     }
-
     render() {
         return (
             <div className="container" id="machine-container">
-
                 {
                     this.state.buttons.map(item => {
-                        return <button className="btn btn-success" key={this.state.buttons.indexOf(item)} onClick={() => this.playSound(this.props.machineSoundNames[this.state.buttons.indexOf(item)])}>{item}</button>
+                        return <div key={this.state.buttons.indexOf(item)}>
+                            <button className="btn btn-success" onClick={() => this.playSound(this.props.machineSoundNames[this.state.buttons.indexOf(item)])}>{item}</button>
+                        </div>
                     })
                 }
-                <LastPlayed soundName={this.state.lastPlayed} />
+                <LastPlayed soundName={this.props.currentSound} />
             </div>
         );
     }
 }
 
-function mapStateToProps(state) {
+const mapStateToProps = state => {
     return {
-        machineSoundNames: state.machineReducer.currentMachineSounds
+        machineSoundNames: state.machineReducer.currentMachineSounds,
+        status: state.machineReducer.AppStatus,
+        currentSound: state.machineReducer.lastSoundPlay
     };
 }
-export default connect(mapStateToProps)(MachineKeys);
+const mapDispatchToProps = dispatch => ({
+    changeLastPlayed: (newSound) => dispatch(actions.changeLastPlayedSound(newSound))
+})
+export default connect(mapStateToProps, mapDispatchToProps)(MachineKeys);
