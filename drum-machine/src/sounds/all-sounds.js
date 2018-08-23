7e010ecdff45b91cf0b335be3fc1e543c03e2b18
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-08-23 10:04:47 +0200

    removed two sounds and added three new sounds.

--- drum-machine/src/sounds/all-sounds.js
+++ drum-machine/src/sounds/all-sounds.js
@@ -3,9 +3,10 @@ var tabla = require("./drum/tabla.mp3");
 var cajonBass = require("./drum/cajon-bass.mp3");
 var hietom = require("./drum/hietom.mp3");
 var hitom = require("./drum/hitom.mp3");
-var marioCoin = require("./drum/mario-coin.mp3");
-var marioJump = require("./drum/mario-jump.wav");
+var crash = require("./drum/Crash.mp3");
+var electricKick = require("./drum/ElecK-Kick.mp3");
 var steel = require("./drum/steel.mp3")
+var snr = require("./drum/Snr.mp3")
 
 var base = require("./guitar/base.mp3");
 var countryChord = require("./guitar/country-chord.mp3");
@@ -21,14 +22,17 @@ var organLow = require("./piano/organ-low.mp3");
 var superCheez = require("./piano/super-cheez.mp3");
 var thickChord = require("./piano/thick-chord.mp3");
 var twoTone = require("./piano/two-tone.mp3");
+var Unknown = require("./Unknown.wav");
 module.exports = {
+    snr,
+    crash,
+    electricKick,
+    Unknown,
     marimba,
     tabla,
     cajonBass,
     hietom,
     hitom,
-    marioCoin,
-    marioJump,
     steel,
     base,
     countryChord,
