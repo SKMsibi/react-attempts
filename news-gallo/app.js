7b1c0f564982820b501537dc73a2f2d244f77e35
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-10-01 09:07:43 +0200

    removed all the unused routes.

--- news-gallo/app.js
+++ news-gallo/app.js
@@ -3,7 +3,7 @@ var express = require("express");
 const Sequelize = require('sequelize');
 const cors = require("cors");
 var bodyParser = require("body-parser");
-var port = process.env.PORT || 3003;
+var port = 3003;
 var loggingMiddleware = require("./log-request-middleware")
 var app = express();
 app.use(cors());
@@ -12,19 +12,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-var {
-  domain,
-  communicationChannelType,
-} = require('./database/models');
-const domainRoutes = require('./routes/domain');
-const contactRequestRoutes = require('./routes/contact-request');
-const adminUserRoutes = require('./routes/admin-user');
-const getOptions = require('./routes/options');
-const checkRequests = require('./database/check-requests');
-domainRoutes(app);
-contactRequestRoutes(app);
-adminUserRoutes(app);
-getOptions(app);
+app.get('/getNewsFeed/:requestData', async (req, res) => {
+  var findNewsFeedOn = req.params["requestData"]
+  console.log("api code working", findNewsFeedOn);
+});
 app.use(function (err, req, res, next) {
   if (err.name === 'UnauthorizedError') {
     res.status(401).send();
