8177c5d716ea0eef4e4a303b9872ad8fbdb8842d
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-10-01 09:54:41 +0200

    added a function that sets the state as text in search box. added function that redirects the user when the search button is clicked

--- news-gallo/news-gallo-app/src/components/landing-page.js
+++ news-gallo/news-gallo-app/src/components/landing-page.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Option from './options';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import '../App.css';
 export class LandingPage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            searchText: "",
+            shouldRedirect: false
+        }
+        this.eventHandler = this.eventHandler.bind(this);
+        this.getNewFeed = this.getNewFeed.bind(this);
+    }
+    getNewFeed() {
+        this.setState({ shouldRedirect: true });
+    }
+    eventHandler(event) {
+        this.setState({ searchText: event.target.value });
+    }
     render() {
+        if (this.state.shouldRedirect) {
+            var location = `/articles/${this.state.searchText}`
+            return <Redirect to={location} />
+        }
         return (
             <div>
                 <Header />
                 <div className="landing-page">
-                    <label>search</label>
-                    <input type="text" />
+                    <input type="text" onChange={this.eventHandler} value={this.state.searchText} />
+                    <label><button onClick={this.getNewFeed}>Search</button></label>
                 </div>
                 <Option />
             </div>
