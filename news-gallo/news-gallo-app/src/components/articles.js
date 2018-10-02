0e76b6bfd6a20f485c5a23f98fa879f367dd1254
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-10-02 13:31:59 +0200

    added functions to navigate to and from the articles and the article-content.

--- news-gallo/news-gallo-app/src/components/articles.js
+++ news-gallo/news-gallo-app/src/components/articles.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header';
 import { connect } from 'react-redux';
 import * as actions from '../actions/all-actions';
+import ArticleContent from './article-content';
 import axios from 'axios';
 import '../App.css';
 
@@ -10,15 +11,25 @@ export class Articles extends Component {
         super(props)
         this.state = {
             subject: this.props.match.params.requestData,
-            allArticles: []
+            allArticles: [],
+            showArticleContent: false,
+            ArticlesContent: ""
         }
+        this.setArticleContent = this.setArticleContent.bind(this);
+        this.HideDisplayContent = this.HideDisplayContent.bind(this);
     }
 
     async componentDidMount() {
         this.props.changeLocation("articles");
         var requestData = this.props.match.params.requestData;
-        const apiRes = await axios.get(`https://newsapi.org/v2/everything?q=${requestData}&from=2018-09-01&sortBy=publishedAt&apiKey=2de6578fa2744b27a5fa386d65a496fd`);
+        const apiRes = await axios.get(`https://newsapi.org/v2/everything?q=${requestData}&from=2018-09-02&sortBy=relevancy&apiKey=2de6578fa2744b27a5fa386d65a496fd`);
         this.setState({ allArticles: apiRes.data.articles })
+    }
+    setArticleContent(content) {
+        this.setState({ showArticleContent: true, ArticleContent: content });
+    }
+    HideDisplayContent() {
+        this.setState({ showArticleContent: false })
     }
     render() {
         return (
@@ -26,11 +37,17 @@ export class Articles extends Component {
                 <Header />
                 <div className="articles-page">
                     <h2>{this.state.subject} Articles</h2>
-                    <div className="articles-list">{this.state.allArticles.map(item => {
-                        return <div>
-                            <h3><img src={item.urlToImage} className="articles-Image" alt="articles-Image" /> {item.title}</h3>
-                        </div>
-                    })}</div>
+                    {!this.state.showArticleContent && (
+                        <div className="articles-list">{this.state.allArticles.map(item => {
+                            return <div className="single-article" onClick={() => this.setArticleContent(item.content)} key={this.state.allArticles.indexOf(item)}>
+                                <img src={item.urlToImage} className="articles-Image" alt="article" />
+                                <h3>{item.title}</h3>
+                            </div>
+                        })}</div>
+                    )}
+                    {this.state.showArticleContent && (
+                        <ArticleContent content={this.state.ArticleContent} backBtn={() => this.HideDisplayContent} />
+                    )}
                 </div>
             </div>
         );
